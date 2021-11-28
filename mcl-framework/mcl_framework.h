2d6a9035a043305117160e53273cf26275734392
bakhtiyork <bakhtiyor.k@gmail.com>
2021-11-28 18:46:56 +0500

    Initial Commit

---
+++ mcl-framework/mcl_framework.h
@@ -0,0 +1,18 @@
+//
+//  mcl_framework.h
+//  mcl-framework
+//
+//  Created by bakhtiyor on 28/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mcl_framework.
+FOUNDATION_EXPORT double mcl_frameworkVersionNumber;
+
+//! Project version string for mcl_framework.
+FOUNDATION_EXPORT const unsigned char mcl_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mcl_framework/PublicHeader.h>
+
+
